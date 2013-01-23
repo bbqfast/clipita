d6b29db50687d2033070d720dce44acfacfa41cd
bbqnow <bbqnow@dell1.(none)>
2013-01-23 09:55:46 -0800

    - save clip id to cookie - facebook scheme button

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -5,6 +5,12 @@ class ClipsController < ApplicationController
       #session[:user_id]=nil
       if (!session[:user_id].nil?) # do not make new clip if signed to facebook
         redirect_to :controller => 'clips', :action => 'index', :clipname=> Clip.clip_name_from_user(current_user)
+        return
+      end
+
+      if(!cookies[:clipname].nil?)
+        redirect_to :controller => 'clips', :action => 'index', :clipname=> cookies[:clipname]
+        return
       end
 
       @clip=Clip.next_clip
@@ -12,8 +18,13 @@ class ClipsController < ApplicationController
 
       #@clip=Clip.save_empty_paste(newname)
       params[:name]=@clip.name
+      # save cookie for next time
+      # Set a simple session cookie
+
+      return
     else
       @clip=Clip.find(:all, :conditions => [ "name = ?", params[:clipname]]).first
+      cookies[:clipname] = @clip.name
     end
   end
   # how to build a view model instead of modelf
